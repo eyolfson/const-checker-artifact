43dd68f376f01be88fee39e881c70f9379031252
Jon Eyolfson <jon@eyl.io>
2019-01-24 14:42:12 -0500

    Added all dependencies to provisioning

--- README.md
+++ README.md
@@ -1,12 +1,53 @@
 # const Checker Artifact
 
+The accompanying artifact for ICSE 2019.
+
 ## Starting the Virtual Machine
 
-    vagrant up
+    vagrant up --provider=virtualbox
     vagrant ssh
 
-## Software
+## Starting the Web Server
 
-- const-checker
-- django-cpp-doc
-- const-checker-experiments
+As the vagrant user:
+
+    cd /home/vagrant/const-checker-experiments-0.1.0
+    python manage.py runserver 0.0.0.0:8000
+
+## Packages
+
+### fish
+
+#### Running
+
+    cd /home/vagrant/const-checker-experiments-0.1.0/fish/2.5.0
+    makepkg
+    cd /home/vagrant/const-checker-experiments-0.1.0
+    scripts/analyze-package.py fish 2.5.0
+
+### LLVM
+
+#### Running
+
+    cd /home/vagrant/const-checker-experiments-0.1.0/llvm/4.0.0
+    makepkg --skippgpcheck
+    cd /home/vagrant/const-checker-experiments-0.1.0
+    scripts/analyze-package.py llvm 4.0.0
+
+### Mosh
+
+#### Running
+
+    cd /home/vagrant/const-checker-experiments-0.1.0/mosh/1.2.6
+    makepkg
+    cd /home/vagrant/const-checker-experiments-0.1.0
+    scripts/analyze-package.py mosh 1.2.6
+
+### Protobuf
+
+#### Running
+
+    cd /home/vagrant/const-checker-experiments-0.1.0/protobuf/3.3.1
+    makepkg --skipchecksums
+    cd /home/vagrant/const-checker-experiments-0.1.0
+    scripts/analyze-package.py protobuf 3.3.1
--- provision.sh
+++ provision.sh
@@ -5,7 +5,7 @@ pacman-key -r 3245467889B6B0C31B668D764D47368BD660A1C7
 pacman-key --lsign-key 3245467889B6B0C31B668D764D47368BD660A1C7
 
 pacman -Syu --noconfirm
-pacman -S base-devel bc bear clang cmake doxygen git htop llvm postgresql python python-django python-psycopg2 wget --noconfirm
+pacman -S base-devel bc bear clang cmake doxygen eigen emacs-nox git gtkglext hdf5 htop intel-tbb libdc1394 libutempter llvm mesa ocaml ocaml-ctypes ocaml-findlib openexr perl-io-tty postgresql protobuf python python-django python-numpy python-psycopg2 python-sphinx python2 python2-numpy python2-setuptools re2c swig unzip wget xine-lib --noconfirm
 
 sudo -u postgres -i initdb -D /var/lib/postgres/data
 systemctl enable postgresql
@@ -13,16 +13,16 @@ systemctl start postgresql
 sudo -u postgres -i createuser --superuser vagrant
 sudo -u vagrant -i createdb cpp_doc
 
-sudo -u vagrant -i wget http://laforge.cs.uwaterloo.ca/const-checker-0.1.0.tar.gz
+sudo -u vagrant -i wget -nv http://laforge.cs.uwaterloo.ca/django-cpp-doc-0.1.0.tar.gz
+sudo -u vagrant -i tar xzf /home/vagrant/django-cpp-doc-0.1.0.tar.gz
+bash -c 'cd /home/vagrant/django-cpp-doc-0.1.0 && python setup.py install'
+
+sudo -u vagrant -i wget -nv http://laforge.cs.uwaterloo.ca/const-checker-experiments-0.1.0.tar.gz
+sudo -u vagrant -i tar xzf /home/vagrant/const-checker-experiments-0.1.0.tar.gz
+sudo -u vagrant -i bash -c 'cd /home/vagrant/const-checker-experiments-0.1.0 && python manage.py migrate'
+
+sudo -u vagrant -i wget -nv http://laforge.cs.uwaterloo.ca/const-checker-0.1.0.tar.gz
 sudo -u vagrant -i tar xzf /home/vagrant/const-checker-0.1.0.tar.gz
 sudo -u vagrant -i bash -c 'mkdir /home/vagrant/const-checker-0.1.0/build && cd /home/vagrant/const-checker-0.1.0/build && cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make'
 bash -c 'cd /home/vagrant/const-checker-0.1.0/build && make install'
 sudo -u vagrant -i psql cpp_doc -c '\i /home/vagrant/const-checker-0.1.0/sql/functions.sql'
-
-sudo -u vagrant -i wget http://laforge.cs.uwaterloo.ca/django-cpp-doc-0.1.0.tar.gz
-sudo -u vagrant -i tar xzf /home/vagrant/django-cpp-doc-0.1.0.tar.gz
-bash -c 'cd /home/vagrant/django-cpp-doc-0.1.0 && python setup.py install'
-
-sudo -u vagrant -i wget http://laforge.cs.uwaterloo.ca/const-checker-experiments-0.1.0.tar.gz
-sudo -u vagrant -i tar xzf /home/vagrant/const-checker-experiments-0.1.0.tar.gz
-sudo -u vagrant -i bash -c 'cd /home/vagrant/const-checker-experiments-0.1.0 && python manage.py migrate'
